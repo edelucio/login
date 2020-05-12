36be716efae61e2bfb4d5272ac6199d5ebf01ea8
Edelucio Luciano <edelucio@gmail.com>
2020-05-11 22:43:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Instalar
+
+$ cd /caminhodoprojeto/login
+$ npm install -g @angular/cli
+$ npm install
+$ ng serve
+
+E acesse a url http://localhost:4200 e o aplicativo estara pronto pra uso.
---
+++ src/app/core/mocks/mock-users.ts
@@ -0,0 +1,8 @@
+import {User} from "../models/user.model";
+
+export const USERS: User[] = [
+  {id: 1, name: 'Admin', surname: '', email: 'admin@gmail.com', username: 'admin', password: 'admin'},
+  {id: 2, name: 'Edelúcio', surname: 'Luciano', email: 'edelucio@gmail.com', username: 'edelucio', password: '12345'},
+  {id: 3, name: 'Adonias', surname: 'Lemos', email: 'adonias@gmail.com', username: 'adonias', password: '12345'},
+  {id: 4, name: 'Roberio', surname: 'Gomes', email: 'atin@somewhere.com', username: 'roberio', password: '12345'},
+];
